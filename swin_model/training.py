1566e51ac177a8578bf196b8a330470f8a1b3aa7
carla-lm <113684954+carla-lm@users.noreply.github.com>
2025-08-21 20:10:35 +0200

    Preliminary Training Script
    Preliminary version of training loop with data loading, transformations, training and validation pipelines and checkpointing

--- swin_model/training.py
+++ swin_model/training.py
@@ -1,17 +1,28 @@
 # Import necessary packages
 import os
 import json
+import time
 import nibabel as nib
 import torch
-import random
 import argparse
 import pytorch_lightning as pl
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-
-matplotlib.use("TkAgg")
 import monai.transforms as T
+from monai.transforms import (AsDiscrete, Activations)
+from monai.losses import DiceLoss
+from monai.inferers import sliding_window_inference
+from monai.metrics import DiceMetric
+from monai.utils.enums import MetricReduction
+from monai.networks.nets import SwinUNETR
 from monai import data
+from monai.data import decollate_batch
+from swinunetr import SwinUNETR3D
+from functools import partial
+
+torch.set_float32_matmul_precision("high")
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 
 def data_loader(batch_size, data_dir, split_file, fold, roi):
@@ -75,6 +86,7 @@ def data_split(data_dir, split_file, fold):
 
 
 def visualize_data(data_dir):
+    matplotlib.use("TkAgg")  # To see figure live
     # Load a random image and label to visualize
     img_path = os.path.join(data_dir, "BraTS2021_00006/BraTS2021_00006_flair.nii.gz")
     label_path = os.path.join(data_dir, "BraTS2021_00006/BraTS2021_00006_seg.nii.gz")
@@ -116,29 +128,212 @@ def visualize_data(data_dir):
     plt.show()
 
 
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = np.where(self.count > 0, self.sum / self.count, self.sum)
+
+
+def save_checkpoint(model, epoch, dir_add, val_acc=0, checkpoint_type="best"):
+    os.makedirs(os.path.dirname(dir_add), exist_ok=True)
+    state_dict = model.state_dict()
+    save_dict = {"epoch": epoch, "val_acc": val_acc, "state_dict": state_dict}
+    if checkpoint_type == "best":
+        path = os.path.join(dir_add, "best_model.pt")
+        torch.save(save_dict, path)
+
+    elif checkpoint_type == "periodic":
+        path = os.path.join(dir_add, f"model_epoch_{epoch}.pt")
+        torch.save(save_dict, path)
+
+    print(f"Saving checkpoint at {dir_add}")
+
+
+def save_training_history(history, file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "w") as f:
+        json.dump(history, f, indent=4)
+
+
+def train_epoch(model, loader, optimizer, scaler, epoch, loss_func):
+    model.train()
+    start_time = time.time()
+    run_loss = AverageMeter()
+    # Use amp if a CUDA device is available
+    use_amp = (device.type == "cuda")
+    for idx, batch_data in enumerate(loader):
+        # Move data to the appropriate device
+        data, target = batch_data["image"].to(device), batch_data["label"].to(device)
+        if use_amp:
+            with torch.amp.autocast('cuda', enabled=use_amp):
+                logits = model(data)
+                loss = loss_func(logits, target)
+
+            # Scale the loss and perform backward pass
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+        else:
+            # Standard training step without AMP
+            logits = model(data)
+            loss = loss_func(logits, target)
+            loss.backward()
+            optimizer.step()
+
+        run_loss.update(loss.item(), n=batch_size)
+        print(
+            "Epoch {}/{} {}/{}".format(epoch+1, epochs, idx+1, len(loader)),
+            "loss: {:.4f}".format(run_loss.avg),
+            "time {:.2f}s".format(time.time() - start_time),
+        )
+        start_time = time.time()
+
+    return run_loss.avg
+
+
+def val_epoch(model, loader, epoch, acc_func, model_inferer=None, post_sigmoid=None, post_pred=None):
+    model.eval()
+    start_time = time.time()
+    run_acc = AverageMeter()
+    use_amp = (device.type == "cuda")
+    with torch.no_grad():
+        for idx, batch_data in enumerate(loader):
+            data, target = batch_data["image"].to(device), batch_data["label"].to(device)
+            if use_amp:
+                with torch.amp.autocast('cuda', enabled=use_amp):
+                    logits = model_inferer(data)
+            else:
+                logits = model_inferer(data)
+
+            # Post-processing steps
+            val_labels_list = decollate_batch(target)
+            val_outputs_list = decollate_batch(logits)
+            val_output_convert = [post_pred(post_sigmoid(val_pred_tensor)) for val_pred_tensor in val_outputs_list]
+
+            # Calculate metric
+            acc_func.reset()
+            acc_func(y_pred=val_output_convert, y=val_labels_list)
+            acc, not_nans = acc_func.aggregate()
+
+            run_acc.update(acc.cpu().numpy(), n=not_nans.cpu().numpy())
+            dice_tc = run_acc.avg[0]
+            dice_wt = run_acc.avg[1]
+            dice_et = run_acc.avg[2]
+
+            print(
+                "Val {}/{} {}/{}".format(epoch+1, epochs/val_every+1, idx+1, len(loader)),
+                ", dice_tc:",
+                dice_tc,
+                ", dice_wt:",
+                dice_wt,
+                ", dice_et:",
+                dice_et,
+                ", time {:.2f}s".format(time.time() - start_time),
+            )
+            start_time = time.time()
+
+    return run_acc.avg
+
+
+def trainer(model, train_loader, val_loader, optimizer, loss_func, acc_func, scheduler, check_dir, model_inferer=None,
+            start_epoch=0, post_sigmoid=None, post_pred=None, checkpoint=True):
+    val_acc_max = 0.0
+    dices_tc, dices_wt, dices_et, dices_avg = [], [], [], []
+    loss_epochs, trains_epoch = [], []
+    scaler = torch.amp.GradScaler(enabled=(device.type == "cuda"))
+    for epoch in range(start_epoch, epochs):
+        print(time.ctime(), "Epoch:", epoch)
+        epoch_time = time.time()
+        train_loss = train_epoch(model, train_loader, optimizer, scaler, epoch=epoch, loss_func=loss_func)
+        print(
+            "Final training  {}/{}".format(epoch+1, epochs),
+            "loss: {:.4f}".format(train_loss),
+            "time {:.2f}s".format(time.time() - epoch_time),
+        )
+
+        if (epoch + 1) % val_every == 0 or epoch == 0:
+            loss_epochs.append(train_loss)
+            trains_epoch.append(int(epoch))
+            epoch_time = time.time()
+            val_acc = val_epoch(model, val_loader, epoch=epoch, acc_func=acc_func, model_inferer=model_inferer,
+                                post_sigmoid=post_sigmoid, post_pred=post_pred)
+            dice_tc, dice_wt, dice_et = val_acc
+            val_avg_acc = np.mean(val_acc)
+            print(
+                "Final validation stats {}/{}".format(epoch+1, epochs),
+                ", dice_tc:", dice_tc, ", dice_wt:", dice_wt, ", dice_et:", dice_et, ", Dice_Avg:", val_avg_acc,
+                ", time {:.2f}s".format(time.time() - epoch_time),
+            )
+            dices_tc.append(dice_tc)
+            dices_wt.append(dice_wt)
+            dices_et.append(dice_et)
+            dices_avg.append(val_avg_acc)
+            if checkpoint and val_avg_acc > val_acc_max:
+                print("new best ({:.6f} --> {:.6f}). ".format(val_acc_max, val_avg_acc))
+                val_acc_max = val_avg_acc
+                save_checkpoint(model, epoch, dir_add=check_dir, val_acc=val_acc_max, checkpoint_type="best")
+
+            if checkpoint and epoch % 10 == 0:
+                save_checkpoint(model, epoch, dir_add=check_dir, val_acc=val_avg_acc, checkpoint_type="periodic")
+
+            scheduler.step()
+
+    print("Training Finished. Best Accuracy: ", val_acc_max)
+    return val_acc_max, dices_tc, dices_wt, dices_et, dices_avg, loss_epochs, trains_epoch
+
+
 if __name__ == '__main__':
+    # Set device to be used (gpu is faster)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    torch.backends.cudnn.benchmark = True
+
     # Set parameters that can be passed by the user
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--lr', type=int, default=48)
+    parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--batch', type=int, default=1)
     parser.add_argument('--embed_dim', type=int, default=48)
-    parser.add_argument('--test', action='store_true')
-    parser.add_argument('--checkpoint', action='store_true')
+    parser.add_argument('--fold', type=int, default=1)
+    parser.add_argument('--roi', type=int, nargs=3, default=[128, 128, 128])
+    parser.add_argument('--val_every', type=int, default=10)
     parser.add_argument('--data', type=str, default='brats')
+    parser.add_argument("--checkpoint", dest="checkpoint", action="store_true")
+    parser.add_argument("--no-checkpoint", dest="checkpoint", action="store_false")
+    parser.set_defaults(checkpoint=True)
     args = parser.parse_args()
 
     # Define hyperparameters
     epochs = args.epochs
     lr = args.lr
     batch_size = args.batch
-    fold = 1
-    roi = (128, 128, 128)
+    fold = args.fold
+    roi = tuple(args.roi)
+    feature_size = args.embed_dim
+    val_every = args.val_every
+    num_heads = [3, 6, 12, 24]
+    window_size = (7, 7, 7)
+    patch_size = (2, 2, 2)
+    embed_dims = [feature_size, feature_size * 2, feature_size * 4, feature_size * 8, feature_size * 16]
+    sw_batch_size = 4
+    infer_overlap = 0.5
 
     # Set relevant paths
     if args.data == 'brats':
         data_dir = os.path.join(os.getcwd(), "TrainingData", "data_brats")  # Path is current directory + data_brats
         split_file = os.path.join(data_dir, "data_split.json")  # Json path is data directory + json filename
+
     if args.data == 'selma':
         data_dir = os.path.join(os.getcwd(), "TrainingData", "data_selma")  # Path is current directory + data_selma
 
@@ -147,3 +342,57 @@ if __name__ == '__main__':
 
     # Visualize data
     # visualize_data(data_dir)
+
+    # Create checkpoint folder unique for each run
+    timestamp = time.strftime("%Y%m%d_%H%M%S")
+    check_dir = os.path.join(os.getcwd(), "checkpoints", timestamp)
+    os.makedirs(check_dir, exist_ok=True)
+
+    # Initialize model
+    model = SwinUNETR3D(
+        in_channels=4,
+        patch_size=patch_size,
+        embed_dims=embed_dims,
+        num_heads=num_heads,
+        window_size=window_size,
+        num_classes=3
+    ).to(device)
+
+    # Monai's model to see if training pipeline works
+    # model = SwinUNETR(
+    #     in_channels=4,
+    #     out_channels=3,
+    #     feature_size=48,
+    #     drop_rate=0.0,
+    #     attn_drop_rate=0.0,
+    #     dropout_path_rate=0.0,
+    #     use_checkpoint=True,
+    # ).to(device)
+
+    # Optimizer and loss function
+    dice_loss = DiceLoss(to_onehot_y=False, sigmoid=True)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)  # Adam with weight decay
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs)  # Decrease following cosine curve
+    post_sigmoid = Activations(sigmoid=True)
+    post_pred = AsDiscrete(argmax=False, threshold=0.5)
+    dice_acc = DiceMetric(include_background=True, reduction=MetricReduction.MEAN_BATCH, get_not_nans=True)
+    model_inferer = partial(
+        sliding_window_inference,
+        roi_size=[roi[0], roi[1], roi[2]],
+        sw_batch_size=sw_batch_size,
+        predictor=model,
+        overlap=infer_overlap,
+    )
+    start_epoch = 0
+    (val_acc_max, dices_tc, dices_wt, dices_et, dices_avg, loss_epochs, trains_epoch) = trainer(
+        model=model, train_loader=train_loader, val_loader=val_loader, optimizer=optimizer,
+        loss_func=dice_loss, acc_func=dice_acc, scheduler=scheduler, model_inferer=model_inferer,
+        start_epoch=start_epoch, post_sigmoid=post_sigmoid, post_pred=post_pred,
+        checkpoint=args.checkpoint, check_dir=check_dir)
+
+    print(f"Training completed, best average dice: {val_acc_max:.4f} ")
+
+    # Save recorded evaluation metrics at the end of the training
+    history_to_save = {"val_acc_max": val_acc_max, "dices_tc": dices_tc, "dices_wt": dices_wt, "dices_et": dices_et,
+                       "dices_avg": dices_avg, "loss_epochs": loss_epochs, "trains_epoch": trains_epoch}
+    save_training_history(history_to_save, os.path.join(check_dir, "training_history.json"))
